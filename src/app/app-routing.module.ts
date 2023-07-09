6bcecc9c9fc9107789af39de9b9db2b765f985f6
spreadcharm <ikitty1128@gmail.com>
2023-07-10 03:04:26 +0900

    add page: top, mypage, mypage/*; add package: angular/material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './top/top.component';
+import { DashboardComponent } from './mypage/dashboard/dashboard.component';
+import { ProfileComponent } from './mypage/profile/profile.component';
+import { InvoiceComponent } from './mypage/invoice/invoice.component';
+import { PlanComponent } from './mypage/plan/plan.component';
+import { InviteComponent } from './mypage/invite/invite.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: TopComponent },
+  { path: "mypage", component: DashboardComponent },
+  { path: "mypage/profile", component: ProfileComponent },
+  { path: "mypage/invoice", component: InvoiceComponent },
+  { path: "mypage/plan", component: PlanComponent },
+  { path: "mypage/invite", component: InviteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { DashboardComponent } from './mypage/dashboard/dashboard.component';
+import { ProfileComponent } from './mypage/profile/profile.component';
+import { InvoiceComponent } from './mypage/invoice/invoice.component';
+import { InviteComponent } from './mypage/invite/invite.component';
+import { PlanComponent } from './mypage/plan/plan.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    DashboardComponent,
+    ProfileComponent,
+    InvoiceComponent,
+    InviteComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
