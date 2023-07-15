30cc92e6b9db19e50d8c5e6d92bad0e0fe19f1e6
spreadcharm <ikitty1128@gmail.com>
2023-07-16 01:26:41 +0900

    add invite page

--- src/app/mypage/invite/invite.component.ts
+++ src/app/mypage/invite/invite.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+export interface TableRow {
+  email: string;
+  role: string;
+  status: string;
+  action: string;
+}
 
 @Component({
   selector: 'app-invite',
   templateUrl: './invite.component.html',
   styleUrls: ['./invite.component.scss'],
 })
-export class InviteComponent {}
+export class InviteComponent {
+  displayedColumns: string[] = ['email', 'role', 'status', 'action'];
+  dataSource: TableRow[] = [].constructor(10).fill({
+    email: 'firstname.lastname@company.name',
+    role: 'メンバー',
+    status: '承認待ち',
+    action: '管理権限を委託する',
+  });
+
+  inviteForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  get inviteFormControl() {
+    return this.inviteForm.controls;
+  }
+
+  onSubmitInviteForm() {
+    console.log(this.inviteForm.value);
+  }
+}
--- src/app/mypage/invoice/invoice.component.ts
+++ src/app/mypage/invoice/invoice.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-export interface PeriodicElement {
+export interface TableRow {
   paymentDay: string;
   plan: string;
   period: string;
@@ -13,16 +13,19 @@ export interface PeriodicElement {
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss'],
 })
-
 export class InvoiceComponent {
-
-  displayedColumns: string[] = ['paymentDay', 'plan', 'period', 'method', 'amount'];
-  dataSource: PeriodicElement[] = [].constructor(10).fill({
-    paymentDay: "2023/4/1",
-    plan: "エンタープライズ - 年額プラン",
-    period: "02月分",
-    method: "口座振替",
-    amount: "50,000円",
+  displayedColumns: string[] = [
+    'paymentDay',
+    'plan',
+    'period',
+    'method',
+    'amount',
+  ];
+  dataSource: TableRow[] = [].constructor(10).fill({
+    paymentDay: '2023/4/1',
+    plan: 'エンタープライズ - 年額プラン',
+    period: '02月分',
+    method: '口座振替',
+    amount: '50,000円',
   });
 }
-
