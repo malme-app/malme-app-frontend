c3be40301eceaaab71a842feb3a240e44ee34c81
spreadcharm <ikitty1128@gmail.com>
2023-08-07 00:06:49 +0900

    modify profile component

--- src/app/mypage/profile/profile.component.ts
+++ src/app/mypage/profile/profile.component.ts
@@ -10,23 +10,20 @@ import { UserInfoService } from 'src/app/providers/user-info.service';
 })
 export class ProfileComponent {
   organizationForm = new FormGroup({
+    organizationType: new FormControl('0', Validators.required),
+    userRole: new FormControl(
+      { value: '0', disabled: true },
+      Validators.required
+    ),
     organizationName: new FormControl('株式会社XXXXXX', Validators.required),
     department: new FormControl('開発部', Validators.required),
     zipcode: new FormControl('000-0000', Validators.required),
     address: new FormControl('東京都千代田区麹町', Validators.required),
     tel: new FormControl('50-1742-2028', Validators.required),
     bankName: new FormControl('三菱UFJ銀行', Validators.required),
-    accountType: new FormControl('普通', Validators.required),
-    accountNumber: new FormControl('1234567', Validators.required),
+    bankAccountType: new FormControl('普通', Validators.required),
+    bankAccountNumber: new FormControl('1234567', Validators.required),
     licenseNumber: new FormControl('5', Validators.required),
-  });
-
-  accountTypeForm = new FormGroup({
-    accountType: new FormControl('0', Validators.required),
-    accountRole: new FormControl(
-      { value: '0', disabled: true },
-      Validators.required
-    ),
   });
 
   constructor(public userInfo: UserInfoService) {}
@@ -34,15 +31,8 @@ export class ProfileComponent {
   get organizationFormControl() {
     return this.organizationForm.controls;
   }
-  get accountTypeFormControl() {
-    return this.accountTypeForm.controls;
-  }
 
   onSubmitOrganizationForm() {
     console.log(this.organizationForm.value);
-  }
-
-  onSubmitAccountTypeForm() {
-    console.log(this.accountTypeForm.value);
   }
 }
