3a45491902d079f7eddb2e4760a0a5210215da80
spreadcharm <ikitty1128@gmail.com>
2023-09-29 23:40:22 +0900

    make plan component work

--- src/app/helper/notificationMessages.ts
+++ src/app/helper/notificationMessages.ts
@@ -1,4 +1,5 @@
 export const MSG_SERVER_ERROR = 'Server error occurred';
+export const MSG_FETCH_FAILED = 'Failed to fetch data';
 export const MSG_CREATE_SUCCESS = 'Successfully created';
 export const MSG_CREATE_FAILED = 'Failed to create data';
 export const MSG_UPDATE_SUCCESS = 'Successfully updated';
@@ -6,3 +7,5 @@ export const MSG_UPDATE_FAILED = 'Failed to update data';
 export const MSG_CANNOT_INVITE = 'This email is already in the other organization';
 export const MSG_INVITE_SUCCESS = 'Successfully invited';
 export const MSG_INVITE_FAILED = 'Failed to invite email';
+export const MSG_APPLY_SUCCESS = 'Successfully applied';
+export const MSG_APPLY_FAILED = 'Failed to apply for the plan';
--- src/app/mypage/plan/plan.component.ts
+++ src/app/mypage/plan/plan.component.ts
@@ -1,8 +1,80 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  MSG_APPLY_FAILED,
+  MSG_APPLY_SUCCESS,
+  MSG_FETCH_FAILED,
+} from 'src/app/helper/notificationMessages';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
   styleUrls: ['./plan.component.scss'],
 })
-export class PlanComponent { }
+export class PlanComponent implements OnInit {
+  plans: any[] = [];
+  lastSale: any = {};
+
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {}
+
+  ngOnInit() {
+    this.fetchLastSale();
+    this.fetchPlans();
+  }
+
+  fetchLastSale() {
+    this.http.get(`${environment.apiBaseUrl}/sale/last`).subscribe({
+      next: (data: any) => {
+        this.lastSale = data;
+        console.log('last sale: ', data);
+      },
+      error: (_error) =>
+        this._snackBar.open(MSG_FETCH_FAILED, 'Close', {
+          horizontalPosition: 'end',
+          verticalPosition: 'top',
+          duration: 5000,
+          panelClass: 'notify-failed',
+        }),
+    });
+  }
+
+  fetchPlans() {
+    this.http.get(`${environment.apiBaseUrl}/plan`).subscribe({
+      next: (data: any) => {
+        this.plans = data;
+        console.log('plans: ', data);
+      },
+      error: (_error) =>
+        this._snackBar.open(MSG_FETCH_FAILED, 'Close', {
+          horizontalPosition: 'end',
+          verticalPosition: 'top',
+          duration: 5000,
+          panelClass: 'notify-failed',
+        }),
+    });
+  }
+
+  applyForPlan(planId: number) {
+    this.http.post(`${environment.apiBaseUrl}/sale`, { planId }).subscribe({
+      next: (data: any) => {
+        this.lastSale = data;
+        console.log('applied for: ', data);
+        this._snackBar.open(MSG_APPLY_SUCCESS, 'Close', {
+          horizontalPosition: 'end',
+          verticalPosition: 'top',
+          duration: 5000,
+          panelClass: 'notify-success',
+        });
+      },
+      error: (_error) =>
+        this._snackBar.open(MSG_APPLY_FAILED, 'Close', {
+          horizontalPosition: 'end',
+          verticalPosition: 'top',
+          duration: 5000,
+          panelClass: 'notify-failed',
+        }),
+    });
+  }
+}
